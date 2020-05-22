21901deefe27deb5cc0c8bd62b96ce6afe5d4dc1
CHAHYUNHO <chamym@naver.com>
2020-05-22 17:32:35 +0900

    App6 add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import App2 from './App2';
 import App3 from './App3';
 import App4 from './App4';
 import App5 from './App5';
+import App6 from './App6';
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
             <Route exact path ="/App3" component={App3}/>
             <Route exact path ="/App4" component={App4}/>
             <Route exact path ="/App5" component={App5}/>
+            <Route exact path ="/App6" component={App6}/>
           </div>
       </Router>  
     );
